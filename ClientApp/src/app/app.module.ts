9d208ed783221be39ff31987cbc23ec085d33add
Lynne Geldenhuys <geldenhuys.lynne@gmail.com>
2023-05-26 17:32:47 +0200

    A bit of styling and branding

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,28 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { PackingFormComponent } from './packing-form/packing-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    PackingFormComponent,
+    FetchDataComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    ReactiveFormsModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
--- ClientApp/src/app/packing-form/packing-form.component.ts
+++ ClientApp/src/app/packing-form/packing-form.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-packing-form',
   templateUrl: './packing-form.component.html',
   styleUrls: ['./packing-form.component.css']
 })
-export class PackingFormComponent {
+export class PackingFormComponent implements OnInit {
+  packingForm: FormGroup = new FormGroup({});
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.packingForm = this.fb.group({
+      weightLimit: ['', Validators.required],
+      items: this.fb.array([])
+    });
+  }
+
+  get items(): FormArray {
+    return this.packingForm.get('items') as FormArray;
+  }
+
+  addItem(): void {
+    const itemGroup = this.fb.group({
+      weight: ['', Validators.required],
+      cost: ['', Validators.required]
+    });
+
+    this.items.push(itemGroup);
+  }
+
+  removeItem(index: number): void {
+    this.items.removeAt(index);
+  }
+
+  submitForm(): void {
+    if (this.packingForm.valid) {
+      // TODO: Implement the logic to send the form data to the server
+      const formValue = this.packingForm.value;
+      console.log(formValue);
+    }
+  }
 }
